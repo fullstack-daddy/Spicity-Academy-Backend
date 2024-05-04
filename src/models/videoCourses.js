e05566a319d5d6192c05589b4a8e0454397c2799
Aboyeji Emmanuel <aboyejiemmanuel07@gmail.com>
2024-05-04 22:22:37 +0100

    DB for video course

---
+++ src/models/videoCourses.js
@@ -0,0 +1,61 @@
+import mongoose from "mongoose";
+
+const AddVideoCourseSchema = new mongoose.Schema({
+  courseTitle: {
+    id: number,
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  courseOrder: {
+    type: number,
+    required: true,
+  },
+  videoLink: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  assignmentQuestion1: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  assignmentQuestion2: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  assignmentQuestion3: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  tablature: {
+    type: image,
+    required: true,
+  },
+  videoThumbnail: {
+    type: String,
+    required: true,
+  },
+  audio: {
+    type: audio,
+    required: true,
+  },
+  loop: {
+    type: audio,
+    required: true,
+  },
+});
+
+export default mongoose.model("addVideoCourse", AddVideoCourseSchema);
